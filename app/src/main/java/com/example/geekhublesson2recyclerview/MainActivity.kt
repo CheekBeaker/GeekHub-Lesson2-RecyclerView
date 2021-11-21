df540a288e6a1f8c6503c95c70b7a9843741b4ba
Denis Bulgakov <cheekbeaker@gmail.com>
2021-11-21 19:16:39 +0200

    Basic layout, and example code done. End of Day1.

--- app/src/main/java/com/example/geekhublesson2recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/geekhublesson2recyclerview/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.geekhublesson2recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.geekhublesson2recyclerview.databinding.ActivityMainBinding
+import ua.motionman.recyclerviewlecture.helper.messageList
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val baseAdapter = RecyclerViewAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initialiseAdapter()
+
+        baseAdapter.messageList = messageList
+    }
+
+    fun initialiseAdapter() {
+        binding.recyclerView.apply {
+            adapter = baseAdapter
+        }
     }
 }
---
+++ app/src/main/java/com/example/geekhublesson2recyclerview/RecyclerViewAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.geekhublesson2recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.geekhublesson2recyclerview.databinding.ItemLeftMessageBinding
+import com.example.geekhublesson2recyclerview.databinding.ItemRightMessageBinding
+import ua.motionman.recyclerviewlecture.model.Message
+
+class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var messageList = emptyList<Message>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when (viewType) {
+            VIEW_TYPE_MESSAGE_LEFT -> {
+                val binding = ItemLeftMessageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderLeftMessage(binding)
+            }
+            VIEW_TYPE_MESSAGE_RIGHT -> {
+                val binding = ItemRightMessageBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ViewHolderRightMessage(binding)
+            }
+            else -> throw IllegalArgumentException()
+        }
+    }
+
+    override fun getItemCount() = messageList.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolderLeftMessage -> holder.bind(messageList[position])
+            is ViewHolderRightMessage -> holder.bind(messageList[position])
+        }
+    }
+
+    class ViewHolderLeftMessage(private val binding: ItemLeftMessageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Message) {
+            binding.firstUserMessageTextView.text = item.message
+        }
+    }
+
+    class ViewHolderRightMessage(private val binding: ItemRightMessageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Message) {
+            binding.secondUserMessageTextView.text = item.message
+        }
+    }
+
+    companion object {
+        const val VIEW_TYPE_MESSAGE_LEFT = 1
+        const val VIEW_TYPE_MESSAGE_RIGHT = 2
+    }
+}
+
